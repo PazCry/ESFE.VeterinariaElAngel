9b8bda9b092cf5a056961f1832f3772d6dd5c6f3
JLiz23 <jencyfranco682@gmail.com>
2025-08-20 00:04:57 -0600

    Arregle diseño de mascota

--- InterfazDeUsuarioUI/VentanaMascota.xaml.cs
+++ InterfazDeUsuarioUI/VentanaMascota.xaml.cs
@@ -24,6 +24,9 @@ namespace InterfazDeUsuarioUI
             InitializeComponent();
         }
 
-      
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
